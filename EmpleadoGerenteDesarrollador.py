ab3cd416ba2675f03fe087b83317ae2223f3ecf7
Mateogi0202 <mateobustamantegiraldo@gmail.com>
2025-09-02 20:31:04 +0000

    primer ejercicio

---
+++ EmpleadoGerenteDesarrollador.py
@@ -0,0 +1,36 @@
+class baseEmpleado:
+    
+    def __init__(self, nombre: str, salario: float, departamento: str):
+        self.nombre = nombre
+        self.salario = salario
+        self.departamento = departamento
+
+class Empleado(baseEmpleado):
+        def __init__(self, nombre: str, salario: float, departamento: str):
+            super().__init__(nombre, salario, departamento)
+        
+        def trabajar(self) -> str:
+            return f"El empleado {self.nombre} está trabajando en el departamento de {self.departamento}."
+
+class Gerente(baseEmpleado): 
+    def __init__(self, nombre: str, salario: float, departamento: str, equipo: list):
+        super().__init__(nombre, salario, departamento)
+        self.equipo = equipo
+
+    def trabajar(self) -> str:
+        return f"El gerente {self.nombre} está gestionando el equipo: {self.equipo}. y su sueldo"
+class desarrollador(baseEmpleado):
+    def  __init__(self, nombre: str, salario: float, departamento: str, lenguaje: str):
+        super().__init__(nombre, salario, departamento)
+        self.lenguaje = lenguaje
+    
+    def trabajar(self) -> str:
+        return f"El desarrollador {self.nombre} está programando en {self.lenguaje}. y el sueldo mensual es de {self.salario}"
+    
+
+mateo = desarrollador ("Mateo", "6.000.000", "Desarrollo", "Python")
+Simon = Gerente ("Simón", "10.000.000", "Ventas", "2")
+melina = Empleado ("Melina", "4.000.000", "Atención al cliente")
+print (mateo.trabajar())
+print (Simon.trabajar())
+print (melina.trabajar())
